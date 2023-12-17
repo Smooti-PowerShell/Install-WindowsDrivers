fe170fd7177636ec8b1d130e53b7d24145a1aa87
smooti <kobeowens43099@gmail.com>
2023-12-16 20:56:53 -0500

    Create mountpoint dir

--- main.ps1
+++ main.ps1
@@ -15,8 +15,12 @@ param (
 
 $offlineMountLocation = "$($PSScriptRoot)\mountPoint"
 
+New-Item -ItemType "directory" -Path $offlineMountLocation
+
 Mount-WindowsImage -ImagePath $ImageLocation -Index $ImageIndex -Path $offlineMountLocation
 
 Add-WindowsDriver -Path $offlineMountLocation -Driver $DriverLocation -Recurse -ErrorAction "SilentlyContinue"
 
 Dismount-WindowsImage -Path $offlineMountLocation -Save
+
+Remove-Item -Path $offlineMountLocation
